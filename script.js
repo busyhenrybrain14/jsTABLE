10dbad447514fa5ad9beea0f8bf805b99fe5bc79
BusyBrain <68847980+busyhenrybrain14@users.noreply.github.com>
2021-05-26 07:45:30 +0100

    Updated by henry

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 //manipalation DOM
 //first i have to create variables
 const numberOfRows = document.getElementById('rows');
-//please crete variable for columns
+Const numberOfColumns = document.getElementById('columns);
 
 const button= document.querySelector('button')
 button.addEventListener("click", createTable);
 
 function createTable(){
   //how many lines? now you can get the values from numberOfRows
+Cell1 = newRow.insertCell(0);
+Cell2 = newRow.insertCell(1);
 
+Cell1.innerHTML = rows;
+Cell2.innerHTML = columns;
   console.log("Labas")
 }
